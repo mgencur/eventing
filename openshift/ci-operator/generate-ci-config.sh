0b9a177921269af7d32fa6621c735c786f09635b
Martin Gencur <mgencur@redhat.com>
2021-09-16 09:17:24 +0200

    Do not use tag_specification in for openshift/release config

--- openshift/ci-operator/generate-ci-config.sh
+++ openshift/ci-operator/generate-ci-config.sh
@@ -40,10 +40,6 @@ EOF
 image_deps=$(print_image_dependencies)
 
 cat <<EOF
-tag_specification:
-  cluster: https://api.ci.openshift.org
-  name: '$openshift'
-  namespace: ocp
 promotion:
   additional_images:
     knative-eventing-src: src
@@ -51,6 +47,16 @@ promotion:
   cluster: https://api.ci.openshift.org
   namespace: openshift
   name: $branch.0
+releases:
+  initial:
+    integration:
+      name: "$openshift"
+      namespace: ocp
+  latest:
+    integration:
+      include_built_images: true
+      name: "$openshift"
+      namespace: ocp
 base_images:
   base:
     name: '$openshift'
